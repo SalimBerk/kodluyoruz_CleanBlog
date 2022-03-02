bc3c86d5a4d92c9efae50af7980dcbb46e42cd74
salim <salimberk2@gmail.com>
2022-03-03 00:30:59 +0300

    03.03.2022

--- app.js
+++ app.js
@@ -1,40 +1,36 @@
 const express = require('express');
-const mongoose=require('mongoose')
-const Post=require('./models/Post')
-const ejs=require('ejs');
+const mongoose = require('mongoose');
+const Post = require('./models/Post');
+const pageControllers = require('./controllers/pageControllers');
+const postControllers = require('./controllers/postControllers');
+var methodOverride = require('method-override');
+const ejs = require('ejs');
 const app = express();
 
-mongoose.connect('mongodb://localhost/AddNewPost')
-app.set("view engine","ejs")
-app.use(express.static('public'))
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
-
-
-app.get('/', async (req, res) => {
-  const posts=await Post.find({})
-  res.render('index',{
-    posts:posts
+mongoose.connect('mongodb://localhost/AddNewPost');
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
   })
-});
-app.get('/posts/:id', async (req, res) => {
-  const post= await Post.findById(req.params.id)
-  res.render('singleposts',{
-    post
-  })
-});
-app.get('/about', (req, res) => {
-  res.render('about')
-});
-app.get('/post', (req, res) => {
-  res.render('post')
-});
+);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.post('/posts',async (req,res)=>{
- await Post.create(req.body)
- res.redirect('/')
-})
+app.get('/', pageControllers.getCratePostPage);
+app.get('/posts/:id', pageControllers.getSinglePostPage);
+app.get('/about', pageControllers.getAboutPage);
+app.get('/post', pageControllers.getPostPage);
 
+app.post('/posts', async (req, res) => {
+  await Post.create(req.body);
+  res.redirect('/');
+});
+app.get('/posts/update/:id', pageControllers.getUpdatePage);
+
+app.put('/posts/:id', postControllers.putPost);
+app.delete('/posts/:id', postControllers.deletePost);
 
 const port = 3000;
 app.listen(port, () => {
